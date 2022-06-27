1eb84febe8965ca52bb3873d29f86676690df4a2
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2022-06-27 20:27:01 +0800

    refactor rlp2json.py: easier to read

--- rlp2json.py
+++ rlp2json.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import sys
 import argparse
 
@@ -10,49 +11,57 @@ def read_hex_stdin():
 def write(s):
     print(s, end = "")
 
-def get_len(l):
-    if l >= 0x38:
-        lenlen = l - 0x38 + 1
-        l = 0
-        for _ in range(lenlen):
-            l = l * 256 + read_byte()
-        return l, lenlen
-    else:
-        return l, 0
+def get_len(ll):
+    l = 0
+    for _ in range(ll):
+        l = l * 256 + read_byte()
+    return l
 
 # return total bytes read
 def parse():
     b = read_byte()
-    if b >= 0xC0:
-        write("[")
-        l, lenlen = get_len(b - 0xC0)
-        todo = l
-        if todo > 0:
-            while True:
-                todo -= parse() 
-                if todo <= 0:
-                    break
-                write(",")
-        write("]")
-    else:
+    if b < 0xC0:
         write("\"")
         if b < 0x80:
-            l, lenlen = 0, 0
+            total = 1
             write("%02x" % b)
         else:
-            l, lenlen = get_len(b - 0x80)
-            for _ in range(l):
+            if b < 0xB8:
+                l = b - 0x80
+                total = 1 + l
+            else:
+                ll = b - 0xB8 + 1   # length of L
+                l = get_len(ll)
+                total = 1 + ll + l
+            for _ in range(l):      # L bytes of content
                 write("%02x" % read_byte())
         write("\"")
-    return 1 + lenlen + l
+    else:
+        write("[")
+        if b < 0xF8:
+            l = b - 0xC0
+            total = 1 + l
+        else:
+            ll = b - 0xF8 + 1       # length of L
+            l = get_len(ll)
+            total = 1 + ll + l
+        if l > 0:                   # L bytes of content
+            l -= parse()
+            while l > 0:
+                write(",")
+                l -= parse()
+        write("]")
+    return total
 
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser(add_help=False)
     arg_parser.add_argument("--help", action="help")
 
     group = arg_parser.add_mutually_exclusive_group()
-    group.add_argument("-h", "--hex", action="store_true", help="hex input (default)")
-    group.add_argument("-b", "--binary", action="store_true", help="binary input")
+    group.add_argument("-h", "--hex",
+        action="store_true", help="hex input (default)")
+    group.add_argument("-b", "--binary",
+        action="store_true", help="binary input")
 
     args = arg_parser.parse_args()
     if args.binary:
